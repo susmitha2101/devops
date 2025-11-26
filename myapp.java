ef02e9a953697a0a376b88868df19b870ddd4a3c
susmitha2101 <susmithareddy2101@gmail.com>
2025-11-26 22:03:05 +0530

    Update myapp.java

--- myapp.java
+++ myapp.java
@@ -1,6 +1,6 @@
 public class Myapp
 {
-	public static void main(string args[])
+	public static void main(String args[])
 	{
 		System.out.println("hello world");
 		System.out.println("hello,good morning");
